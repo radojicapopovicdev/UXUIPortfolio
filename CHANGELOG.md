f16bd3eb06511fff6a856aedce6ea8b973a1288f
Radojica Popovic <radojicapopovic@icloud.com>
2024-03-02 20:46:25 +0100

    - Basic Structure

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
 ### UX/UI Designer Portfolio
 ## Changelog
 
+### 02.03.2024
+### 0.0.2
 
+# Added
+
+- Basic Structure
 
 ### 02.03.2024
 ### 0.0.1
---
+++ components/SocialProof.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SocialProof = (props: Props) => {
+  return (
+    <div>SocialProof</div>
+  )
+}
+
+export default SocialProof
